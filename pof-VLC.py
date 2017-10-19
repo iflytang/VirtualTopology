424d3167026ada08421ef6dc32275aa73b3eccb3
iflytang <iflytang@qq.com>
2017-10-18 23:42:17 -0700

    test for pof-VLC

--- pof-VLC.py
+++ pof-VLC.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
@@ -8,75 +9,54 @@ from mininet.clean import cleanup
 from mininet.node import RemoteController
 from mininet.pof import POFSwitch
 from mininet.topolib import TreeTopo
+
 CONTROLLER_LISTEN_PORT = 6643
+
 class Test(Topo):
     '''
-    The network has 6 switches.
-    
-          s6-cn
-           |
-          s5
-         /   \
-        s3---s4
-        |     |
-    mn1-s2    s1-mn2		
+
+    The network has 3 swithes, 2 hosts and 1 controller.
+    This topology is used for testing pof-VLC.
+          cn
+          |
+         sw3
+         /  \
+      sw2   sw1(AP)
+   (wire)\   /(wireless)
+          mn
     '''
-    def __init__(self, n = 6, **opts):
+
+    def __init__(self, n = 3, **opts):
         super(Test, self).__init__(**opts)
-        
-        switchList = [self.addSwitch('s%d' % i, listenPort = (CONTROLLER_LISTEN_PORT + i)) for i in range(1,n+1)]
-        lastSwitch = None
-       # for switch in switchList:
-        #    if lastSwitch:
-         #       self.addLink(lastSwitch, switch)
-          #  lastSwitch = switch
-	cn=self.addHost('cn');
-	mn1=self.addHost('mn1');
-	mn2=self.addHost('mn2');
-	    #self.addLink(cn,switchList[5]);
-	    #self.addLink(mn1,switchList[1]);
-	    #self.addLink(mn2,switchList[0]);
-        #self.addLink(switchList[0], switchList[3])
-        #self.addLink(switchList[1], switchList[2])
-        #self.addLink(switchList[2], switchList[3])
-        #self.addLink(switchList[2], switchList[4])
-	    #self.addLink(switchList[4], switchList[3])
-	    #self.addLink(switchList[4], switchList[5])
-		
-	self.addLink(mn1, switchList[1])
-	self.addLink(switchList[1], switchList[2])
-	self.addLink(switchList[4], switchList[3])
-	self.addLink(switchList[2], switchList[4])
-	self.addLink(switchList[2], switchList[3])
-	self.addLink(mn2, switchList[0])
-	self.addLink(switchList[0], switchList[3])
-	self.addLink(switchList[4], switchList[5])
-	self.addLink(cn, switchList[5])
-		
-        
 
+        # add switches
+        sw1 = self.addSwitch('s1', listenPort = CONTROLLER_LISTEN_PORT + 1)
+        sw2 = self.addSwitch('s2', listenPort = CONTROLLER_LISTEN_PORT + 2)
+        sw3 = self.addSwitch('s3', listenPort = CONTROLLER_LISTEN_PORT + 3)
 
-        #for j in range(n):
-         #   host = self.addHost('h%d' % (j+1))
-          #  self.addLink(host, switchList[j])
+        # add hosts
+        cn = self.addHost('cn')
+        mn = self.addHost('mn')
+
+        # add links
+        self.addLink(sw3, sw2) # sw3-port1 == sw2-port1
+        self.addLink(sw3, sw1) # sw3-port2 == sw1-port1
+        self.addLink(sw3, cn)  # sw3-port3 == cn
+        self.addLink(sw2, mn)  # sw2-port2 == mn
+        self.addLink(sw1, mn)  # sw1-port2 == mn
+
 if __name__ == '__main__':
-    
     cleanup()
-    
-    topo6 = Test(6)          
+    topo6 = Test(3)
     net = Mininet(topo=topo6, switch=POFSwitch)
-    net.addController('c0', controller = RemoteController, ip='192.168.109.123', port=CONTROLLER_LISTEN_PORT)
-    
+    net.addController('c0', controller = RemoteController, ip='192.168.109.111', port=CONTROLLER_LISTEN_PORT)
     setLogLevel('info')
+
     net.start()
     net.staticArp()
     print "Dumping host connections ..."
+
     #dumpNodeConnections(net.hosts)
     dumpNodeConnections(net.switches)
     CLI(net)
     net.stop()
-
-
-
-
-
